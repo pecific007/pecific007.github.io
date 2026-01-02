99fd64a73d1cf9147ce7b37e9a10089d5a6f28d8
Prashant <invizableprash@gmail.com>
2026-01-02 14:40:39 +0530

    Fixed scroll-snapping on mobile view and added separate list for titles

--- art.js
+++ art.js
@@ -1,6 +1,32 @@
 const template = document.querySelector('#card_template');
 const content = document.querySelector('.content');
-const list = {
+
+const total_images = 20;
+
+const title_text = {
+    1: "Blackhole - 1",
+    2: "Blackhole - 2",
+    3: "Blackhole - 3",
+    4: "Hornet from Hollow Knight",
+    5: "Mechanical keyboard",
+    6: "A plant",
+    7: "viewport plant",
+    8: "A bedroom",
+    9: "Viewport bedroom",
+    10: "Old Computer with cellshading",
+    11: "Livimg room with cellshading",
+    12: "A livingroom",
+    13: "Crusader helment - 1",
+    14: "cursader helment",
+    15: "Heart",
+    16: "Pelvic girdle",
+    17: "Kitchen - 1",
+    18: "Kitchen - 2",
+    19: "Kitchen - 3",
+    20: "Kitchen - 4"
+};
+
+const alt_text = {
     1: "A 3D render of blackhole",
     2: "A 3D render of blackhole",
     3: "A 3D render of blackhole",
@@ -23,19 +49,21 @@ const list = {
     20: "A 3D render of a kitchen"
 };
 
-let text = Object.values(list);
-for (let i of text) {
+let text_alt = Object.values(alt_text);
+let text_title = Object.values(title_text);
+
+for (let i = 0; i < total_images; i++) {
     content.innerHTML += template.innerHTML;
 }
 
 let l = 1;
 document.querySelectorAll('.image').forEach(img => {
     img.src = `Images/${l}.png`;
-    img.alt = text[l-1];
+    img.alt = text_alt[l-1];
     l++;
 })
 l=0;
 document.querySelectorAll('.title').forEach(title => {
-    title.textContent = text[l];
+    title.textContent = text_title[l];
     l++;
 })
