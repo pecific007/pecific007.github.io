d9c7070b7833ed2b2c5509731d308afc7b124ec4
Prashant <invizableprash@gmail.com>
2026-04-04 16:26:27 +0530

    Fixed JS in art and socials

--- art.js
+++ art.js
@@ -1,56 +1,43 @@
-const template = document.querySelector('#card_template');
-const content = document.querySelector('.content');
+const template = document.querySelector("#card_template");
+const content = document.querySelector(".content");
 
 const list_all = {
-    "":"",
-    "Blackhole - 1" : "A 3D render of blackhole",
-    "Blackhole - 2" : "A 3D render of blackhole",
-    "Blackhole - 3" : "A 3D render of blackhole",
-    "Hornet from Hollow Knight" : "A 3D render of Hornet from Hollow Knight",
-    "Mechanical keyboard" : "A 3d render of a mechanical keyboard",
-    "A plant" : "A 3D render of a plant",
-    "viewport plant" : "A 3D viewport of a plant",
-    "A bedroom" : "A 3D render of a bedroom",
-    "Viewport bedroom" : "A 3D viewport of a bedroom",
-    "Old Computer with cellshading" : "A 3D render of computer with cellshading",
-    "Livimg room with cellshading" : "A 3D render of a livimg room with cellshading",
-    "A livingroom" : "A 3D render of a livingroom",
-    "Crusader helment - 1" : "A 3D render of crusader helment",
-    "cursader helment - 2" : "A 3D render of cursader helment",
-    "Heart" : "A pencil drawing of heart",
-    "Pelvic girdle" : "A pencil drawing of the pelvic girdle",
-    "Kitchen - 1" : "A 3D render of a kitchen",
-    "Kitchen - 2" : "A 3D render of a kitchen",
-    "Kitchen - 3" : "A 3D render of a kitchen",
-    "Kitchen - 4" : "A 3D render of a kitchen",
-    " ":" "
+  "Blackhole_-_1": "A 3D render of blackhole",
+  "Blackhole_-_2": "A 3D render of blackhole",
+  "Blackhole_-_3": "A 3D render of blackhole",
+  Hornet_from_Hollow_Knight: "A 3D render of Hornet from Hollow Knight",
+  Mechanical_keyboard: "A 3d render of a mechanical keyboard",
+  A_plant: "A 3D render of a plant",
+  Viewport_plant: "A 3D viewport of a plant",
+  A_bedroom: "A 3D render of a bedroom",
+  Viewport_bedroom: "A 3D viewport of a bedroom",
+  Old_Computer_with_cellshading: "A 3D render of computer with cellshading",
+  Livimg_room_with_cellshading: "A 3D render of a livimg room with cellshading",
+  A_livingroom: "A 3D render of a livingroom",
+  "Crusader_helmet_-_1": "A 3D render of crusader helment",
+  "Crusader_helmet_-_2": "A 3D render of cursader helment",
+  Heart: "A pencil drawing of heart",
+  Pelvic_girdle: "A pencil drawing of the pelvic girdle",
+  "Kitchen_-_1": "A 3D render of a kitchen",
+  "Kitchen_-_2": "A 3D render of a kitchen",
+  "Kitchen_-_3": "A 3D render of a kitchen",
+  "Kitchen_-_4": "A 3D render of a kitchen",
+};
+
+const title_text = Object.keys(list_all);
+
+for (let path in list_all) {
+  el = document.createElement("div");
+  el.classList.add("card_all");
+  el.innerHTML = template.innerHTML;
+  content.appendChild(el);
+  img = el.querySelector("img");
+  img.src = `Images/${path}.png`;
+  img.alt = list_all[path];
+  el.querySelector(".title").textContent = path.replaceAll("_", " ");
+  if (path == "Blank_100px") {
+    el.style.visibility = "hidden";
+    el.ariaHidden = "true";
+  }
 }
-
-const title_text = Object.keys(list_all); 
-const alt_text = Object.values(list_all); 
-
-const total_images = title_text.length;
-
-
-for (let i = 0; i < total_images; i++) {
-    content.innerHTML += template.innerHTML;
-}
-
-let l = 0;
-document.querySelectorAll('.image').forEach(img => {
-    img.src = `Images/${l}.png`;
-    img.alt = alt_text[l];
-    l++;
-});
-l=0;
-document.querySelectorAll('.title').forEach(title => {
-    title.textContent = title_text[l];
-    l++;
-});
-l=0;
-const all_cards = document.querySelectorAll('.card');
-all_cards[0].style.opacity = "0";
-all_cards[21].style.opacity = "0";
-if (window.innerWidth <= 700) {
-    all_cards[0].remove() 
-}
+content.scrollTo(160, 0);
