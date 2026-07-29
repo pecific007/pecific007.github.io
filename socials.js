80af408c10b983dfdeb9584311bc5ab31857f229
pecific007 <pecific2007@gmail.com>
2026-07-29 19:32:02 +0530

    Added: new links to /socials (wordpress site & /weather_react), tried fixing the scrolling issue on mobile in /art

---
+++ socials.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+      const container = document.querySelector('.card_container');
+      const template = document.querySelector("#card_template");
+      // All links to display:
+      const icons = {
+        "GitLab": "https://gitlab.com/Pecific007",
+        "Github": "https://github.com/pecific007",
+        "YouTube": "https://youtube.com/@prash_rathod007",
+        "Instagram": "https://www.instagram.com/endframe_art/",
+        "Wordpress": "https://pecific007.wordpress.com",
+        "Weather": "/weather_react",
+        "MonkeyType": "https://monkeytype.com/profile/Pecific007",
+        "Sketchfab": "https://sketchfab.com/Minute_watchers_64"
+      }
+
+      for (name in icons) {
+        el = document.createElement('label');
+        el.classList.add('card')
+        el.innerHTML = template.innerHTML;
+        img = el.querySelector('img');
+        img.src = `Images/icons/${name}.svg`;
+        img.alt = `Logo: ${name}`;
+          if (name == "MonkeyType") {
+            img.style.width = "6rem";
+            img.style.paddingInline = ".5rem";
+          }
+          else if (name == "GitLab") {
+            img.style.width = "6rem";
+            img.style.height = "6rem";
+            img.style.backgroundColor = "#6650edAA";
+            img.style.borderRadius = "15px";
+            img.style.paddingInline = ".8rem";
+          }
+        btn = el.querySelector('button');
+        btn.textContent = name;
+        link = el.querySelector('a');
+        link.href = icons[name];
+        container.appendChild(el);
+      }
+});
