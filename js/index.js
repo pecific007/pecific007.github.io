10e295d12dfedd1828e58305546f287e72014139
pecific007 <pecific2007@gmail.com>
2026-08-09 22:37:36 +0530

    Refactored: Loading 'current-projects' section in index.html dynamically through js using github's api

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
 export function init() {
-  console.log("Index page");
+  fetch("https://api.github.com/users/pecific007/repos?sort=updated&per_page=5")
+    .then (response => response.json())
+    .then(repos => {
+      const container = document.querySelector(".section-projects");
+
+      repos.forEach(repo => {
+        const tr = document.createElement("tr");
+        console.log(tr);
+        const td = document.createElement("td");
+        td.classList.add("projects-item");
+        const link = document.createElement("a");
+        link.href = repo.html_url;
+        link.target = "_blank";
+        const button = document.createElement("button");
+        button.classList.add("blue_button");
+        button.textContent = repo.name;
+        link.appendChild(button);
+        td.appendChild(link);
+        tr.appendChild(td);
+        container.appendChild(tr);
+      })
+    })
 }
